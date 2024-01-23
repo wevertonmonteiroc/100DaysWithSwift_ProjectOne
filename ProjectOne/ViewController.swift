06852c840fba502ee5e2f9ceb3f76813dd26442e
Weverton <wevertonmonteiroc@gmail.com>
2024-01-22 22:20:34 -0300

    I used the FileManager library to read the images that I added and will use in the project.

--- ProjectOne/ViewController.swift
+++ ProjectOne/ViewController.swift
@@ -2,16 +2,28 @@
 //  ViewController.swift
 //  ProjectOne
 //
-//  Created by admin on 22/01/24.
+//  Created by Weverton Monteiro on 22/01/24.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        //TODO: Remover o force unwrap
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
     }
 
 
